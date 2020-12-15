36f0515cc57c3858293dd1636f9718c64e928cc7
Sam Fox <samueljasonfox@gmail.com>
2020-12-14 22:39:19 -0800

    countdown to game start solved

--- Assets/script.js
+++ Assets/script.js
@@ -5,17 +5,18 @@ console.log('linked!');
 var body = document.body;
 
 //var to later create all necessary elements
-var btnEl0 = document.createElement('button');
+var btnElNav = document.createElement('button');
 var btnEl1 = document.createElement('button');
 var btnEl2 = document.createElement('button');
 var btnEl3 = document.createElement('button');
 var btnEl4 = document.createElement('button');
 
 
-// variable to focue on id tags header
+// variable to focue on id tags h1
 var nav = document.querySelector('#nav')
-var mainH1 = document.querySelector('#header')
+var h1 = document.querySelector('#h1')
 var pTag1 = document.querySelector('#pTag1')
+var pTag2 = document.querySelector('#pTag2')
 var buttonBin = document.querySelector('#button-bin')
 var wrongBtn = document.querySelector('#wrong')
 var correctBtn = document.querySelector('#correct')
@@ -29,66 +30,64 @@ homePage()
 // HOME PAGE
 function homePage() {
     // add nav button
-    nav.appendChild(btnEl0)
+    nav.appendChild(btnElNav)
     //set new nav button attribute
-    btnEl0.setAttribute('id', 'navBtnTxt')
+    btnElNav.setAttribute('id', 'navBtnTxt')
     //set nav button attribute text
     navBtnTxt.textContent = "Score Card"
     navBtnTxt.addEventListener('click', function () {
+        console.log('Scores Page')
         scoresPage()
     })
 
-    //add text content of header
-    mainH1.textContent = 'Quiz Time';
+    //add text content of h1
+    h1.textContent = 'Quiz Time';
     // add text content of p element
-    pTag1.textContent = 'press button to start game'
+    pTag1.textContent = 'Game Rules go here'
 
-    //create start button element
-    //set id attribute to start button element
-    btnEl1.setAttribute('id', 'start-button')
-    //append start button to page
-    buttonBin.appendChild(btnEl1)
-    // add start button id
-    startBtn = document.querySelector('#start-button')
-    startBtn.textContent = 'Start'
+    //start button
+    //add to page and set attribute id
+    buttonBin.appendChild(btnEl1).setAttribute('id', 'start-button')
+    //set attribute onclick function to start game when button is clicked
+    btnEl1.setAttribute('onclick', 'gameCountdown()')
+    //set button text
+    startButtonText = document.querySelector('#start-button')
+    startButtonText.textContent = 'Start'
 
-    startBtn.addEventListener('click', function () {
-        //remove nav button on game start
-        nav.innerHTML = ''
-        question1()
-    })
     //homepage footer
     footer.textContent = 'Samuel Fox, December 2020';
     footer.style.textAlign = 'center';
 }
 
+//Start countdown timer
+function gameCountdown() {
 
-// SCORE PAGE
-function scoresPage() {
-    //header text
-    mainH1.textContent = 'Scores'
-    // cleat pTag text
-    pTag1.textContent = ""
-    //clear button box
-    buttonBin.innerHTML = ''
+    //start countdown timer 3 seconds
+    var timeLeft = 3;
 
-    //set nav button attribute text
-    navBtnTxt.textContent = "Home"
-    navBtnTxt.addEventListener('click', function () {
-        homePage()
-    })
+    var timeInterval = setInterval(function () {
+        startButtonText.textContent = '* ' + timeLeft + ' *';
+        timeLeft--;
 
-    //remove footer during game mode?
+        if (timeLeft === -1) {
+            //reset button bin
+            buttonBin.innerHTML = ''
+            question1();
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
+
+
+    //lauch game start
+
 }
-
-
-
 
 
 // START GAME / question 1 layout of 'main content'
 function question1() {
-    //add text content of header
-    mainH1.textContent = 'Question 1';
+    //add text content of h1
+    h1.textContent = 'Question 1';
     // add text content of p element
     pTag1.textContent = 'ask question here'
     //create button element in our button bin
@@ -117,12 +116,35 @@ function question1() {
 
 }
 
+
+
+
 //onclick function for wrong answer
 function wrong() {
     console.log('wrong')
 }
-
 //onclick function for correct answer
 function correct() {
     console.log('correct')
+}
+
+
+
+
+// SCORE PAGE
+function scoresPage() {
+
+    //h1 text
+    h1.textContent = 'Scores'
+    // cleat pTag text
+    pTag1.textContent = ""
+    //clear button box
+    buttonBin.innerHTML = ''
+
+    //set nav button attribute text
+    navBtnTxt.textContent = "Home"
+    navBtnTxt.addEventListener('click', function () {
+        console.log('Home Page')
+        homePage()
+    })
 }
