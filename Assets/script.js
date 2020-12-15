c639122cb1a30cd4f7fd95b201796b52ed2bfdee
Sam Fox <samueljasonfox@gmail.com>
2020-12-14 21:42:56 -0800

    added onclick events to listen for correct answer. Also to doc time if wrong answer is chosen

--- Assets/script.js
+++ Assets/script.js
@@ -5,11 +5,11 @@ console.log('linked!');
 var body = document.body;
 
 //var to later create all necessary elements
-btnEl0 = document.createElement('button');
-btnEl1 = document.createElement('button');
-btnEl2 = document.createElement('button');
-btnEl3 = document.createElement('button');
-btnEl4 = document.createElement('button');
+var btnEl0 = document.createElement('button');
+var btnEl1 = document.createElement('button');
+var btnEl2 = document.createElement('button');
+var btnEl3 = document.createElement('button');
+var btnEl4 = document.createElement('button');
 
 
 // variable to focue on id tags header
@@ -17,19 +17,16 @@ var nav = document.querySelector('#nav')
 var mainH1 = document.querySelector('#header')
 var pTag1 = document.querySelector('#pTag1')
 var buttonBin = document.querySelector('#button-bin')
-
-//nav link home / scores
-
+var wrongBtn = document.querySelector('#wrong')
+var correctBtn = document.querySelector('#correct')
 var footer = document.querySelector('#footer')
 
-//footer text and styling
-footer.textContent = 'Samuel Fox, December 2020';
-footer.style.textAlign = 'center';
 
-//lauch program at main page
+
+// LOAD HOME PAGE
 homePage()
 
-// home page layout of 'main content'
+// HOME PAGE
 function homePage() {
     // add nav button
     nav.appendChild(btnEl0)
@@ -40,7 +37,6 @@ function homePage() {
     navBtnTxt.addEventListener('click', function () {
         scoresPage()
     })
-
 
     //add text content of header
     mainH1.textContent = 'Quiz Time';
@@ -59,13 +55,18 @@ function homePage() {
     startBtn.addEventListener('click', function () {
         //remove nav button on game start
         nav.innerHTML = ''
-        startGame()
+        question1()
     })
+    //homepage footer
+    footer.textContent = 'Samuel Fox, December 2020';
+    footer.style.textAlign = 'center';
 }
 
+
+// SCORE PAGE
 function scoresPage() {
     //header text
-    mainH1.textContent = 'Recent Scores'
+    mainH1.textContent = 'Scores'
     // cleat pTag text
     pTag1.textContent = ""
     //clear button box
@@ -77,41 +78,51 @@ function scoresPage() {
         homePage()
     })
 
+    //remove footer during game mode?
 }
 
 
-// start game / question 1 layout of 'main content'
-function startGame() {
+
+
+
+// START GAME / question 1 layout of 'main content'
+function question1() {
     //add text content of header
     mainH1.textContent = 'Question 1';
-
     // add text content of p element
     pTag1.textContent = 'ask question here'
-
     //create button element in our button bin
-    //set id attribute to button element
-    btnEl1.setAttribute('id', 'wrong-1')
-    //append button to page
-    buttonBin.appendChild(btnEl1)
-    // add id
-    button = document.querySelector('#wrong-1')
-    button.textContent = 'wrong 1'
 
-    btnEl2.setAttribute('id', 'wrong-2')
-    buttonBin.appendChild(btnEl2)
-    button = document.querySelector('#wrong-2')
-    button.textContent = 'wrong 2'
+    //wrong button 1
+    //add button to page & add id attribute
+    buttonBin.appendChild(btnEl1).setAttribute('id', 'wrong-1')
+    //set attribute onclick function
+    btnEl1.setAttribute('onclick', 'wrong()')
+    //focus on attribute & set text content
+    document.querySelector('#wrong-1').textContent = 'wrong 1'
 
-    btnEl3.setAttribute('id', 'wrong-3')
-    buttonBin.appendChild(btnEl3)
-    button = document.querySelector('#wrong-3')
-    button.textContent = 'wrong 3'
+    //wrong button 2
+    buttonBin.appendChild(btnEl2).setAttribute('id', 'wrong-2')
+    btnEl2.setAttribute('onclick', 'wrong()')
+    document.querySelector('#wrong-2').textContent = 'wrong 2'
+    //wrong button 3
+    buttonBin.appendChild(btnEl3).setAttribute('id', 'wrong-3')
+    btnEl3.setAttribute('onclick', 'wrong()')
+    document.querySelector('#wrong-3').textContent = 'wrong 3'
 
-    btnEl4.setAttribute('id', 'correct')
-    buttonBin.appendChild(btnEl4)
-    button = document.querySelector('#correct')
-    button.textContent = 'correct'
-
+    //correct button
+    buttonBin.appendChild(btnEl4).setAttribute('id', 'correct')
+    btnEl4.setAttribute('onclick', 'correct()')
+    document.querySelector('#correct').textContent = 'correct'
 
 }
 
+//onclick function for wrong answer
+function wrong() {
+    console.log('wrong')
+}
+
+//onclick function for correct answer
+function correct() {
+    console.log('correct')
+}
